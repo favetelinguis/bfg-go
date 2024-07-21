4b90dfce01d9d3153009be65c376cc1370ec6282
Henrik Larsson <t1@henkelarsson.se>
2024-07-21 19:02:21 +0200

    add frappe tui theme

---
+++ tui/theme.go
@@ -0,0 +1,25 @@
+package tui
+
+import "github.com/charmbracelet/lipgloss"
+
+// Based on frappe from
+// https://github.com/catppuccin/catppuccin
+const (
+	Rosewater = lipgloss.Color("#f2d5cf")
+	Flamingo  = lipgloss.Color("#eebebe")
+	Pink      = lipgloss.Color("#f4b8e4")
+	Mauve     = lipgloss.Color("#ca9ee6")
+	Red       = lipgloss.Color("#e78284")
+	Maroon    = lipgloss.Color("#ea999c")
+	Peach     = lipgloss.Color("#ef9f76")
+	Yellow    = lipgloss.Color("#e5c890")
+	Green     = lipgloss.Color("#a6d189")
+	Teal      = lipgloss.Color("#81c8be")
+	Sky       = lipgloss.Color("#99d1db")
+	Sapphire  = lipgloss.Color("#85c1dc")
+	Blue      = lipgloss.Color("#8caaee")
+	Lavender  = lipgloss.Color("#babbf1")
+	Text      = lipgloss.Color("#c6d0f5")
+	SubText1  = lipgloss.Color("#b5bfe2")
+	SubText0  = lipgloss.Color("#a5adce")
+)
